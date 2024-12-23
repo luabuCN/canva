91751b360ee97d7ce48ecde09d0ea0f269365c9b
luabuCN <157495717+luabuCN@users.noreply.github.com>
2024-12-23 11:24:02 +0800

    ci: 更新 Unsplash API 密钥配置
    - 移除了本地配置中的硬编码 API 密钥
    - 尝试使用环境变量 NEXT_PUBLIC_UNSPLASH_ACCESS_KEY
    - 如果环境变量未设置，将回退到硬编码的 API 密钥

--- src/lib/unsplash.ts
+++ src/lib/unsplash.ts
@@ -1,11 +1,7 @@
 import { createApi } from "unsplash-js";
 
 export const unsplash = createApi({
-<<<<<<< HEAD
   // accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!, vercel 部署不生效
   accessKey: "WhmMbg_TC32FaHGel-Lpc1sFMbfEOemNCCpMGDkjgcM",
-=======
-  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!,
->>>>>>> 7d17db0f8eac8e468aea769a1a7fbcb35f3cd8df
   fetch: fetch,
 });
