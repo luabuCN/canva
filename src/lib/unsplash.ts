bbd6f7c1a6d573b921f9c76214aeaf03a27d7e78
luabuCN <157495717+luabuCN@users.noreply.github.com>
2024-12-24 11:14:57 +0800

    build: 恢复 Unsplash API 访问密钥配置
    - 重新使用环境变量 NEXT_PUBLIC_UNSPLASH_ACCESS_KEY 作为访问密钥
    - 注释掉硬编码的访问密钥，以提高代码的安全性和可维护性

--- src/lib/unsplash.ts
+++ src/lib/unsplash.ts
@@ -1,7 +1,7 @@
 import { createApi } from "unsplash-js";
 
 export const unsplash = createApi({
-  // accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!, vercel 部署不生效
-  accessKey: "WhmMbg_TC32FaHGel-Lpc1sFMbfEOemNCCpMGDkjgcM",
+  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!,
+  // accessKey: "WhmMbg_TC32FaHGel-Lpc1sFMbfEOemNCCpMGDkjgcM",
   fetch: fetch,
 });
