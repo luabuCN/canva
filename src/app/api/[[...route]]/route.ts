66b6676f2eb080002c3fc3ae3d06d93eb3144267
luabuCN <157495717+luabuCN@users.noreply.github.com>
2025-01-08 14:49:15 +0800

    refactor(app): 修改获取认证密钥方式
    - 将获取认证密钥的方式从 Context 对象改为直接从环境变量中获取
    - 这种修改提高了代码的可移植性和安全性，因为环境变量是更常用和安全的配置方式

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -9,7 +9,7 @@ export const runtime = "nodejs";
 
 function getAuthConfig(c: Context): AuthConfig {
   return {
-    secret: c.env.AUTH_SECRET,
+    secret: process.env.AUTH_SECRET,
     ...(authConfig as any),
   };
 }
