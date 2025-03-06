542b3d445bcc062b977aa648cf7048bd71789d2b
luabuCN <157495717+luabuCN@users.noreply.github.com>
2025-03-06 17:31:56 +0800

    feat(navbar): 在导航栏添加 GitHub 图标链接
    - 在导航栏组件中添加 GitHub 图标
    - 图标链接指向项目的 GitHub 仓库
    - 鼠标悬停时图标透明度降低，实现过渡效果

--- src/app/[locale]/(dashboard)/navbar.tsx
+++ src/app/[locale]/(dashboard)/navbar.tsx
@@ -1,10 +1,18 @@
 import UserButton from "@/app/features/auth/components/user-button";
 import LocaleSwitch from "@/components/localeSwitch";
+import { Github } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className="w-full flex items-center p-4 h-[68px]">
       <div className="ml-auto gap-4 flex items-center justify-center">
+        <a
+          href="https://github.com/luabuCN/canva"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github className="hover:opacity-75 transition-opacity" />
+        </a>
         <UserButton />
         <LocaleSwitch />
       </div>
